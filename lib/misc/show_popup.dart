c770d8d98201c8627f5bc82bc892a81f39bcce6e
Kdwk <kdwkleung@proton.me>
2024-10-21 10:33:13 +0800

    show_popup: change colour of sheet

--- lib/misc/show_popup.dart
+++ lib/misc/show_popup.dart
@@ -3,11 +3,13 @@ import 'package:gomate/widgets/other/adaptive.dart';
 import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
 void Function() showPopup(BuildContext context, {required Widget Function(BuildContext) sheet}) {
+  final theme = Theme.of(context);
   return () {
     WoltModalSheet.show(
       context: context,
       pageListBuilder: (context) => [
         SliverWoltModalSheetPage(
+          backgroundColor: theme.colorScheme.surfaceContainerHighest,
           hasTopBarLayer: false,
           mainContentSliversBuilder: (context) => [
             SliverList.list(children: [
